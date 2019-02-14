aa681601cbc5a4ea6d8abeac8995b4daa778adc1
Nathan <NDJAG3@gmail.com>
2019-02-13 20:40:45 -0600

    Base working w/ Instructions | 2/13/19

--- app/src/main/java/Test/com/Database.java
+++ app/src/main/java/Test/com/Database.java
@@ -16,7 +16,7 @@ public class Database
         jeff[2] = "Hammer";
         jeff[3] = "Bread";
         jeff[4] = "Egg";
-        jeff[5] = "Wall";
+        jeff[5] = "Unrecognised Object";
 
 //night night theme theme
         return jeff[a];
@@ -24,19 +24,20 @@ public class Database
     public static String getInfo(int a)
     {
         String jeff[] = new String[6];
-        jeff[0] = "Wrench in the plans";
-        jeff[1] = "Screwdriver It";
-        jeff[2] = "Hammer Time";
+        String h = "How to use:\n\n";
+        jeff[0] = h + "1.) Grab wrench in one hand\n2.) Fit nut in the notch\n3.) Turn counterclockwise to loosen the bolt/nut";
+        jeff[1] = h + "1.) Hold the screwdriver\n2.) Place the notch in the screw\n3.) Turn counterclockwise to loosen the bolt/nut";
+        jeff[2] = h + "1.) Firmly grasp hammer in one hand\n2.) Swing so that the metal part hits the intended object\n3.) Repeat first two steps until satisfied";
         jeff[3] = "Breadies";
         jeff[4] = "Eggies";
-        jeff[5] = "Bunch of Bricks" ;
-                //jeff[5] is just for kicks and giggles
+        jeff[5] = "Please Retry Image Search";
+
         return jeff[a];
     }
     public static int getImg(int a)
     {
         int jeff[] = new int[6];
-        Arrays.fill(jeff, R.drawable.kirbster);
+        Arrays.fill(jeff, R.drawable.question_mark);
         jeff[0] = R.drawable.img0;
         jeff[1] = R.drawable.img1;
         jeff[2] = R.drawable.img2;
