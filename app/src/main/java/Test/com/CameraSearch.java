6229dcd34d7c8ceae32c84de4e60c6b2482ac88c
Nathan <NDJAG3@gmail.com>
2019-02-13 21:11:47 -0600

    Merge branch 'NN_Testing' of https://github.com/S-m1tty/AR-Object-Identification into NN_Testing
    # Conflicts:
    #	app/src/main/java/Test/com/Database.java

--- app/src/main/java/Test/com/CameraSearch.java
+++ app/src/main/java/Test/com/CameraSearch.java
@@ -2,13 +2,81 @@ package Test.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class CameraSearch extends AppCompatActivity {
+
+    TextView eggTextView;
+    TextView breadTextView;
+    Button   startProcessBtn;
+    Bitmap bitmp = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_search);
+
+        bitmp = BitmapFactory.decodeResource(getResources(), R.drawable.kirbster);
+
+        eggTextView = (TextView) findViewById(R.id.eggTextView);
+        breadTextView = (TextView) findViewById(R.id.breadTextView);
+        startProcessBtn = (Button)findViewById(R.id.startProcessBtn);
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+    public void process(View v) {
+        if (bitmp == null) {
+            Toast.makeText(this, "no bitmap", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this, Integer.toString(bitmp.getByteCount()), Toast.LENGTH_SHORT).show();
+
+        ImageClassifier imgclass = null;
+        try {
+            imgclass = new ImageClassifier(this);
+        } catch (IOException e) {
+            Toast.makeText(this, "failed to make classifier", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+        if (imgclass == null) {
+            Toast.makeText(this, "classifier is null!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ArrayList<NNOutput> array = imgclass.classifyFrame(bitmp);
+
+        NNOutput egg = array.get(0);
+        NNOutput bread = array.get(1);
+        //setBreadMulti((int) Math.round(bread.getValue()*100));
+        //setEggMulti((int) Math.round(egg.getValue()*100));
+
+        //TextView output = findViewById(R.id.output);
+        //output.setText(array.get(0).toString()+"\n"+array.get(1).toString());
+        eggTextView.setText(array.get(0).toString());
+        breadTextView.setText(array.get(1).toString());
+
+    }
+
 }
--- app/src/main/java/Test/com/Database.java
+++ app/src/main/java/Test/com/Database.java
@@ -11,12 +11,13 @@ public class Database
     public static String getName(int a)
     {
         String jeff[] = new String[6];
-        jeff[0] = "Wrench";
-        jeff[1] = "Screwdriver";
-        jeff[2] = "Hammer";
-        jeff[3] = "Bread";
-        jeff[4] = "Egg";
-        jeff[5] = "Unrecognised Object";
+        jeff[0] = "Unrecognised Object";
+        jeff[1] = "Hammer";
+        jeff[2] = "Screwdriver";
+        jeff[3] = "Wrench";
+        jeff[4] = "Bread";
+        jeff[5] = "Egg";
+
 
 //night night theme theme
         return jeff[a];
@@ -25,22 +26,27 @@ public class Database
     {
         String jeff[] = new String[6];
         String h = "How to use:\n\n";
-        jeff[0] = h + "1.) Grab wrench in one hand\n2.) Fit nut in the notch\n3.) Turn counterclockwise to loosen the bolt/nut";
-        jeff[1] = h + "1.) Hold the screwdriver\n2.) Place the notch in the screw\n3.) Turn counterclockwise to loosen the bolt/nut";
-        jeff[2] = h + "1.) Firmly grasp hammer in one hand\n2.) Swing so that the metal part hits the intended object\n3.) Repeat first two steps until satisfied";
-        jeff[3] = "Breadies";
-        jeff[4] = "Eggies";
-        jeff[5] = "Please Retry Image Search";
+
+        jeff[0] = "Please Retry Image Search";
+        jeff[1] = h + "1.) Firmly grasp hammer in one hand\n2.) Swing so that the metal part hits the intended object\n3.) Repeat first two steps until satisfied";
+        jeff[2] = h + "1.) Hold the screwdriver\n2.) Place the notch in the screw\n3.) Turn counterclockwise to loosen the bolt/nut";
+        jeff[3] = h + "1.) Grab wrench in one hand\n2.) Fit nut in the notch\n3.) Turn counterclockwise to loosen the bolt/nut";
+        jeff[4] = "Breadies";
+        jeff[5] = "Eggies";
 
         return jeff[a];
     }
     public static int getImg(int a)
     {
         int jeff[] = new int[6];
+
         Arrays.fill(jeff, R.drawable.question_mark);
-        jeff[0] = R.drawable.img0;
+
+
         jeff[1] = R.drawable.img1;
         jeff[2] = R.drawable.img2;
+        jeff[3] = R.drawable.img3;
+
         //Do more for more items
 
         return jeff[a];
